e5b238baef0392d33e1458ea8f3ce489d3fc57ca
Nicole <NicoleLKitson@gmail.com>
2019-01-20 20:29:35 -0500

    Added ruby item

--- src/main/java/com/liquidmemories/liquidspace/util/Reference.java
+++ src/main/java/com/liquidmemories/liquidspace/util/Reference.java
@@ -1,9 +1,9 @@
 package com.liquidmemories.liquidspace.util;
 
-public class Reference {
-
-	public static final String MOD_ID = "sm";
-	public static final String NAME = "Space Mod";
+public class Reference 
+{
+	public static final String MOD_ID = "liquidspace";
+	public static final String NAME = "Liquid Space";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.liquidmemories.liquidspace.proxy.ClientProxy";
